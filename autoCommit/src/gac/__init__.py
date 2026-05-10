5f2fd21febef13d0406b282475792ab1eda8172d
t-kabaya <taikikabaya@gmail.com>
2026-05-10 15:40:42 +0700

    feat: __author__の名前をKabaya Taikiに変更

--- autoCommit/src/gac/__init__.py
+++ autoCommit/src/gac/__init__.py
@@ -4,5 +4,5 @@ Local LLM-powered git commit message generator.
 """
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Kabaya Taiki"
 __all__ = ["cli", "config", "git_utils", "llm", "prompts", "installer"]
